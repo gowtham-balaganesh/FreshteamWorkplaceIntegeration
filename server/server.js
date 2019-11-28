48952019e9210ebb30182f806263a5fee3f620b6
gogulaanand-rammohan <gogulaanand.rammohan@freshworks.com>
2019-11-28 15:08:57 +0530

    Update initialized

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ function request(options) {
 
 function requester(options){
 
-  requestNPM.post(options, function(error, response, body){
+  requestNPM(options, function(error, response, body){
     console.log("error",error);
     console.log("body",body);
 
@@ -49,12 +49,12 @@ function requester(options){
 exports = {
 
   events: [
-    { event: 'onEmployeeCreate', callback: 'onEmployeeCreateHandler' }
-    // { event: 'onEmployeeUpdate', callback: 'onEmployeeUpdateHandler' }
+    { event: 'onEmployeeCreate', callback: 'onEmployeeCreateHandler' },
+    { event: 'onEmployeeUpdate', callback: 'onEmployeeUpdateHandler' }
   ],
 
   onEmployeeCreateHandler: function(args) {
-  	const external_id = "2311";
+  	var external_id = "23";
   	const options = {
   		method:'POST',
   		url:'https://graph.facebook.com/company/accounts',
@@ -67,7 +67,7 @@ exports = {
 			  "title"        : args["data"]["employee"]["designation"],
 			  "department"   : args["data"]["employee"]["department"],
 			  "organization" : "Freshworks",
-			  "manager"      : args["data"]["associations"]["reporting_to"]["user_emails"][0],
+			  "manager"      : "15tuee047@skct.edu.in",//args["data"]["associations"]["reporting_to"]["user_emails"][0],
 			  "division"     : "Freshdesk",	
 			  "cost_center"  : "test",
 			  "external_id"  : external_id,
@@ -79,11 +79,11 @@ exports = {
     requester(options);
     external_id = Number(external_id)+1;
     external_id.toString();
-  }//,
+  },
 
   onEmployeeUpdateHandler: function(args) {
   	const email = args["data"]["employee"]["user_emails"][0]
-  	const addr = "https://graph.facebook.com/"+email
+  	const addr = "https://graph.facebook.com/" + email
   	const options = {
   		method:'GET',
   		url:addr,
@@ -91,38 +91,38 @@ exports = {
   			Authorization: `Bearer DQVJ2RzNSRXlKQmNuRnYwaTMwT1lWM0pxZAVlPOGhzV0EwamVQNGxSRFVEOUxsMU5Od19sLUNNVVdOOXpQLWQwT0RvUDNleExnUkswaDQxY3N5WGRCMGhMTnNNNk9UbHZAZAc0J0cVBQaUE1QVl5VFZA4eWZAFYVg2MmQ1ckZAIVmFScjF2NUdjR1NtUjNYRnFQLTU4eGE0bWFHT0VBcTNCNXFHeVNfdXZAKU2hYLVBZAczEyVHdyMXE3bEJocmdnQjdLbnRyOTRjUFFVcVk0UWNlUXNUMwZDZD}`
   		}
     }
-    resp = requester(options);
+    resp = requester(options)
     console.log(resp);
+    var id = resp["id"]
+
   }
 
 
 
 
 
-
-  	// const args["data"]["employee"][""]
-  	// const options = {
-  	// 	method:'PUT',
-  	// 	url:"https://graph.facebook.com/"+"/",
-  	// 	headers:{
-  	// 		Authorization: `Bearer DQVJ2RzNSRXlKQmNuRnYwaTMwT1lWM0pxZAVlPOGhzV0EwamVQNGxSRFVEOUxsMU5Od19sLUNNVVdOOXpQLWQwT0RvUDNleExnUkswaDQxY3N5WGRCMGhMTnNNNk9UbHZAZAc0J0cVBQaUE1QVl5VFZA4eWZAFYVg2MmQ1ckZAIVmFScjF2NUdjR1NtUjNYRnFQLTU4eGE0bWFHT0VBcTNCNXFHeVNfdXZAKU2hYLVBZAczEyVHdyMXE3bEJocmdnQjdLbnRyOTRjUFFVcVk0UWNlUXNUMwZDZD}`
-  	// 	},
-  	// 	form:{
-			//   "name"         : args["data"]["employee"]["first_name"],
-			//   "email"        : args["data"]["employee"]["user_emails"][0],
-			//   "title"        : args["data"]["employee"]["designation"],
-			//   "department"   : args["data"]["employee"]["department"],
-			//   "organization" : "Freshworks",
-			//   "manager"      : "15tuee047@skct.edu.in",
-			//   "division"     : "Freshdesk",	
-			//   "cost_center"  : "test",
-			//   "external_id"  : "2309",
-			//   "invited"      : true,
-			//   "work_locale"  : args["data"]["associations"]["branch"]["location"],
-			//   "auth_method"  : "password"
-			// }
-   //  }
-  //   requester(options);
-  // }
+  	const options = {
+  		method:'PUT',
+  		url:"https://graph.facebook.com/" + id,
+  		headers:{
+  			Authorization: `Bearer DQVJ2RzNSRXlKQmNuRnYwaTMwT1lWM0pxZAVlPOGhzV0EwamVQNGxSRFVEOUxsMU5Od19sLUNNVVdOOXpQLWQwT0RvUDNleExnUkswaDQxY3N5WGRCMGhMTnNNNk9UbHZAZAc0J0cVBQaUE1QVl5VFZA4eWZAFYVg2MmQ1ckZAIVmFScjF2NUdjR1NtUjNYRnFQLTU4eGE0bWFHT0VBcTNCNXFHeVNfdXZAKU2hYLVBZAczEyVHdyMXE3bEJocmdnQjdLbnRyOTRjUFFVcVk0UWNlUXNUMwZDZD}`
+  		},
+  		form:{
+			  "name"         : args["data"]["employee"]["first_name"],
+			  "email"        : args["data"]["employee"]["user_emails"][0],
+			  "title"        : args["data"]["employee"]["designation"],
+			  "department"   : args["data"]["employee"]["department"],
+			  "organization" : "Freshworks",
+			  "manager"      : "15tuee047@skct.edu.in",
+			  "division"     : "Freshdesk",	
+			  "cost_center"  : "test",
+			  "external_id"  : "2309",
+			  "invited"      : true,
+			  "work_locale"  : args["data"]["associations"]["branch"]["location"],
+			  "auth_method"  : "password"
+			}
+    }
+    requester(options);
+  }
 
 };
