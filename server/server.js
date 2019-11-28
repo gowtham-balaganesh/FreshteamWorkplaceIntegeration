218149668bbca77430218eae5daf127e50df9077
gogulaanand-rammohan <gogulaanand.rammohan@freshworks.com>
2019-11-28 20:27:37 +0530

    Add update function

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const requestNPM = require('request');
+const SUCCESS_CODES = [200,201,202];
 
 function cleanOptions(options) {
     return JSON.stringify(options, function (key, value) {
@@ -16,7 +17,7 @@ function cleanOptions(options) {
 function request(options) {
   console.log("requsr function")
     return new Promise((resolve, reject) => {
-        requestNPM.post(options.url, options ,function (error, response, body) {
+        requestNPM(options ,function (error, response, body) {
           console.log("respnse",response)
             if (error) {
                 return reject(
@@ -36,11 +37,24 @@ function request(options) {
     });
 }
 
+function getOrg() {
+    const addr = "https://graph.facebook.com/community"
+    const options = {
+      method:'GET',
+      url:addr,
+      headers:{
+        Authorization: bearer_token
+      }
+    }
+    resp = requester(options)
+    console.log(resp);
+    return resp
+  }
 
 function requester(options){
 
   requestNPM(options, function(error, response, body){
-    console.log("error",error);
+    // console.log("error",error);
     console.log("body",body);
 
   }) 
@@ -101,7 +115,7 @@ exports = {
   		form:{
 			  "name"         : args["user"]["first_name"],
 			  "email"        : args["user"]["email"],
-			  "phone"				 : [ args["user_additional_details"][0]["work_numbers"][0]["number"] ],
+			  "phone_number" : "919789468802",
 			  "department"   : department_name,
 			  "title"        : args["user"]["designation"],
 			  "division"     : team_name,	
@@ -109,12 +123,13 @@ exports = {
 			  "manager"      : manager_email_id,		
 			  "start_date"   : args["user"]["joining_date"],
 			  "invited"      : true,
-			  "work_locale"  : "en_IN",
+			  "work_address" : "Chennai",
+			  "locale"       : "en_US",
 			  "auth_method"  : "password"
 			}
     }
     console.log(options["form"])
-    //requester(options);
+    return request(options);
   },
 
   onEmployeeUpdateHandler: function(args) {
@@ -124,33 +139,43 @@ exports = {
   		method:'GET',
   		url:addr,
   		headers:{
-  			Authorization: `Bearer DQVJ2RzNSRXlKQmNuRnYwaTMwT1lWM0pxZAVlPOGhzV0EwamVQNGxSRFVEOUxsMU5Od19sLUNNVVdOOXpQLWQwT0RvUDNleExnUkswaDQxY3N5WGRCMGhMTnNNNk9UbHZAZAc0J0cVBQaUE1QVl5VFZA4eWZAFYVg2MmQ1ckZAIVmFScjF2NUdjR1NtUjNYRnFQLTU4eGE0bWFHT0VBcTNCNXFHeVNfdXZAKU2hYLVBZAczEyVHdyMXE3bEJocmdnQjdLbnRyOTRjUFFVcVk0UWNlUXNUMwZDZD}`
+  			Authorization: bearer_token
   		}
     }
-    resp = requester(options)
     var id = resp["id"]
-  	var options = {
-  		method:'PUT',
-  		url:"https://graph.facebook.com/" + id,
-  		headers:{
-  			Authorization: `Bearer DQVJ2RzNSRXlKQmNuRnYwaTMwT1lWM0pxZAVlPOGhzV0EwamVQNGxSRFVEOUxsMU5Od19sLUNNVVdOOXpQLWQwT0RvUDNleExnUkswaDQxY3N5WGRCMGhMTnNNNk9UbHZAZAc0J0cVBQaUE1QVl5VFZA4eWZAFYVg2MmQ1ckZAIVmFScjF2NUdjR1NtUjNYRnFQLTU4eGE0bWFHT0VBcTNCNXFHeVNfdXZAKU2hYLVBZAczEyVHdyMXE3bEJocmdnQjdLbnRyOTRjUFFVcVk0UWNlUXNUMwZDZD}`
-  		},
-  		form:{
-			  "name"         : args["user"]["name"],
-			  "email"        : args["user"]["email"],
-			  "phone"        : args["user_personal_details"][0]["phone_numbers"][0]["number"],
-			  "department"   : args["data"]["employee"]["department"],
-			  "title"        : args["data"]["employee"]["designation"],
-			  "department"   : args["data"]["employee"]["department"],
-			  "organization" : "Freshworks",
-			  "manager"      : agrs["user_emails"][1]["email"],
-			  "division"     : "Freshdesk",
-			  "invited"      : true,
-			  "work_locale"  : args["branch"]["location"],
-			  "auth_method"  : "password"
-			}
+    resp = requester(options)
+    if(args["user"]["terminated"] == true || args["user"]["deleted"] == true){
+      var options = {
+        method:'DELETE',
+        url:"https://graph.facebook.com/"+id+"?active=false",
+        headers:{
+          Authorization: bearer_token
+        }
+      }
     }
-    requester(options);
+    else{
+    	var options = {
+    		method:'PUT',
+    		url:"https://graph.facebook.com/" + id,
+    		headers:{
+    			Authorization: bearer_token
+    		},
+    		form:{
+  			  "name"         : args["user"]["name"],
+  			  "email"        : args["user"]["email"],
+  			  "phone"        : args["user_personal_details"][0]["phone_numbers"][0]["number"],
+  			  "department"   : args["data"]["employee"]["department"],
+  			  "title"        : args["data"]["employee"]["designation"],
+  			  "department"   : args["data"]["employee"]["department"],
+  			  "organization" : "Freshworks",
+  			  "manager"      : agrs["user_emails"][1]["email"],
+  			  "division"     : "Freshdesk",
+  			  "invited"      : true,
+  			  "work_locale"  : args["branch"]["location"],
+  			  "auth_method"  : "password"
+  			}
+      }
+    requester(options);}
   }
 
 };
